a6cff99ca1564f31932f6da3c60b0aed246c1544
Daniel <damelikyan@gmail.com>
2024-04-26 15:04:04 -0700

    add error tests

--- errors_test.go
+++ errors_test.go
@@ -15,16 +15,16 @@ func TestErrors(t *testing.T) {
 		httpmock.Activate()
 		defer httpmock.DeactivateAndReset()
 
+		testError := Error{
+			Code:    "TOKEN_USED",
+			Message: "Token has already been used",
+		}
+
 		httpmock.RegisterResponder(http.MethodPost, urlClassify,
 			func(req *http.Request) (*http.Response, error) {
 				return httpmock.NewJsonResponse(http.StatusOK, classifyResponse{
 					errorResponse: errorResponse{
-						Errors: []Error{
-							{
-								Code:    "TOKEN_USED",
-								Message: "Token has already been used",
-							},
-						},
+						Errors: []Error{testError},
 					},
 				})
 			},
@@ -34,22 +34,31 @@ func TestErrors(t *testing.T) {
 		if !errors.Is(err, ErrTokenUsed) {
 			t.Errorf("Expected error %q, got %v", ErrTokenUsed, err)
 		}
+
+		var apiErr Error
+		if !errors.As(err, &apiErr) {
+			t.Errorf("Expected error to be of type %T", apiErr)
+		}
+
+		if apiErr != testError {
+			t.Errorf("Expected error %v, got %v", testError, apiErr)
+		}
 	})
 
 	t.Run("error unknown token", func(t *testing.T) {
 		httpmock.Activate()
 		defer httpmock.DeactivateAndReset()
 
+		testError := Error{
+			Code:    "TOKEN_NOT_FOUND",
+			Message: "Token not found",
+		}
+
 		httpmock.RegisterResponder(http.MethodPost, urlClassify,
 			func(req *http.Request) (*http.Response, error) {
 				return httpmock.NewJsonResponse(http.StatusOK, classifyResponse{
 					errorResponse: errorResponse{
-						Errors: []Error{
-							{
-								Code:    "TOKEN_NOT_FOUND",
-								Message: "Token not found",
-							},
-						},
+						Errors: []Error{testError},
 					},
 				})
 			},
@@ -58,6 +67,15 @@ func TestErrors(t *testing.T) {
 		_, err := client.Classify("token")
 		if !errors.Is(err, ErrTokenNotFound) {
 			t.Errorf("Expected error %q, got %v", ErrTokenUsed, err)
+		}
+
+		var apiErr Error
+		if !errors.As(err, &apiErr) {
+			t.Errorf("Expected error to be of type %T", apiErr)
+		}
+
+		if apiErr != testError {
+			t.Errorf("Expected error %v, got %v", testError, apiErr)
 		}
 	})
 }
